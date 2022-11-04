6483508b1fd9f334335016905a39a167eff34559
CaueSouza <caue.souzabarros@hotmail.com>
2022-11-04 00:34:22 -0300

    Creating new responses

--- resposes.py
+++ resposes.py
@@ -1,4 +1,9 @@
 def handle_response(message) -> str:
     p_message = message.lower()
 
-    return 'I\'m still being developed, thanks for understanding :)'
+    if p_message == "help":
+        return "Help sendo construido, guenta ai"
+    elif p_message == "ping":
+        return "pong"
+    else:
+        return 'Não conheço esse comando seu imbecil, da uma lida no ?help'
